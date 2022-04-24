397613508a4c98070d0ec3b7ab3aa6aa8de18203
Ruslan <ruslan@0787@mail.ru>
2022-04-24 15:40:29 +0300

    add solution from JavaSyntax task 0403

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task04.task0403;
+
+import java.util.Scanner;
+
+/*
+Суммирование
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int sum = 0;
+        boolean isEnter = false;
+        while(!isEnter){
+            String string = scan.nextLine();
+            isEnter = string.equals("ENTER");
+            if(isEnter){
+                break;
+            }
+           
+            sum = sum + Integer.parseInt(string);
+            
+        }
+       
+            System.out.println(sum);
+    }
+}
