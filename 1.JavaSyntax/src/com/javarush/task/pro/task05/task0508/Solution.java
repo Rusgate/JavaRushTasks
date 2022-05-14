635c6ef72164485e5042e6902b929781d6196399
Ruslan <ruslan@0787@mail.ru>
2022-05-14 14:05:38 +0300

    add solution from JavaSyntax task 0405

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
@@ -1,9 +1,8 @@
 package com.javarush.task.pro.task05.task0508;
 
 import java.util.Scanner;
- import java.util.*;
 
-/*
+/* 
 Удаляем одинаковые строки
 */
 
@@ -11,39 +10,25 @@ public class Solution {
     public static String[] strings;
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String[] strings= new String[6];
-        for(int j = 0; j < strings.length; j++) {
-            strings[j]=scan.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        strings = new String[6];
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = scanner.nextLine();
         }
-            for(int j = 0; j < strings.length; j++) {
-            for(int i = 0; i < strings.length; i++){
-              }
-                if (  strings[i]==null; strings[j]==null; j==i){
-                        continue;
-
-                 }else if(strings[j].equals(strings[i])){
-                  strings[j]=  null;
-                   strings[i]=  null;
-                    }
+        for (int i = 0; i < strings.length-1; i++) { // null; null; null; Java; Tasks; World // i = 3
+                String word = strings[i]; // word = null
+            for (int j = i + 1; j < strings.length; j++) { // j=2
+                if(word==null) {
+                    break;
                 }
-                   }
-//              if(strings[1].equals(strings[2])){
-//                  strings[1]=  null;
- //                  strings[2]=  null;
- //                   }
- //          if(strings[2].equals(strings[3])){
- //                 strings[2]=  null;
-  //                  strings[3]=  null;
- //                   }
- //           if(strings[3].equals(strings[4])){
- //                 strings[3]=  null;
- //                  strings[4]=  null;
-//                    }
-//                    if(strings[4].equals(strings[5])){
- //                 strings[4]=  null;
-//                    strings[5]=  null;
-//                    }
+                if (word.equals(strings[j])) { //
+                    strings[i] = null;
+                    strings[j] = null;
+                }
+
+            }
+        }
+
         for (int i = 0; i < strings.length; i++) {
             System.out.print(strings[i] + ", ");
         }
